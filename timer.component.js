43f758ce93a0df7776bcdfad54f91a4e064781a6
Jahnavi <157940845+Jahnavi-Pennabadi@users.noreply.github.com>
2024-09-11 08:57:14 +0530

    Timer

---
+++ timer.component.js
@@ -0,0 +1,12 @@
+export class timer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Game Start</h1>
+                <span>00:</span>
+                <span>00:</span>
+                <span>01</span>
+            </div>
+        )
+    }
+}
